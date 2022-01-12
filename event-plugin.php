1125d9b4688112d3363e493d0548a0be669650b2
Sanjay Subba <sanjay@sochware.com>
2022-01-13 00:09:39 +0545

    first commit

---
+++ event-plugin.php
@@ -0,0 +1,8 @@
+<?php
+/**
+* Plugin Name: Event Plugin
+* Author: Sanjay Subba
+* Author URI: https://www.facebook.com/sanjeev.limboo.3
+* Description: This is simple plugin for generating Event CPT.
+* Version: 1.0
+**/
